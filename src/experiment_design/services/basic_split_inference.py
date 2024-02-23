6b843b7b0a9b5addfba628de2025e0e09908ab38
Steve Piccolo <71099580+StevePic95@users.noreply.github.com>
2024-02-23 09:19:33 -0500

    Version 0.3.0 (#16)
    * fix SSH connection logic
    
    * zero-deploy RPC working for basic tasks
    
    * custom implementation of rpyc.zerodeploy.DeployedServer
    
    Big change in directory structure to fit better with the zerodeploy experiment setup process. An experiment is a list of node-types mapped to physical devices available on the network. A node-type is really just an RPyC service that will get zerodeployed to the physical device.
    
    * Change directory structure and add demo directory.
    
    * Progress on basic demo
    
    * Observer exposes a torch Dataset object defined by user
    
    * BaseDataLoader implements all required methods except __next__ and __init__
    
    * commit before push
    
    * About to delete some files
    
    * Reorganize directories
    
    * Edit gitignore to leave the TestResults dir empty
    
    * small edits to WrappedModel to work with thread-safe dict
    
    * Runner looking good
    
    * Task definitions outlined
    
    * Runner and Task classes solid
    
    * Fix outdated documentation
    
    * Fix zerodeploy script (no longer include DataRetriever or $ROLE$)
    
    * Update gitignore
    
    * change directory structure and fix imports
    
    * fix gitignore
    
    * start implementing the AlexNet test case using the new system (finally)
    
    * Decouple Delegator vs. Executor
    
    * Delegator-Executor relationship defined
    
    * Clean commit before integrating the new api
    
    * Add remote logger to participant servers
    
    * Fix local handler for remote log records
    
    * Apart from saving test results as csv, rough implementation done
    
    * Just on a bughunt now
    
    * fix zerodeploy remote script
    
    * clean commit
    
    * absorb Runners into NodeServices
    
    The node apis are much easier to work with if the delegator/executor functionality just becomes part of the NodeService classes
    
    * gitignore a test directory
    
    * user no longer has to subclass BaseExecutor to define participant node behavior
    
    * Experiment class uses threading.Event objects to orchestrate nodes
    
    * Fix thread logic
    
    * Add a self-destruct event for zerodeployed nodes
    
    * Pretty sure it's just a matter of fixing the remote loggers now
    
    * Remote logging works!
    
    * checkpoint
    
    * AM meeting push
    
    * mother of god it just worked!
    
    * Checkpoint after meeting with Nick
    
    Have to collect logs locally on each remote device to capture stdout and stderr
    
    * checkpoint
    
    * relocated forward exit point
    
    * unpack split input internally
    
    * Add ipynb for last report
    
    * checkpoint before fixing masterdict bottleneck
    
    Simple tests are working reliably now, but still slow. I want to try to fix the bottleneck so we can run larger tests.
    
    * masterdict bottleneck is fixed!
    
    Somewhat successful test on a larger dataset, and much faster. Just need to fix the way data is consolidated to the final report dataframe.
    
    * Testbed is working. Here's a good checkpoint commit.
    
    * Hardcoded some values for test - needs updating
    
    * update config details and testcase yaml file for Onion
    
    modify the config.yaml by adding the separate calssification yaml details for both balanced and unbalanced. Modify the code accordingly. Added yolosplit.yaml for test cases
    
    * cleanup
    
    Fix filepaths after slight directory change
    
    update readme
    
    * update readme
    
    * Add gif for README
    
    * render new gif
    
    * stage 1 refactor
    
    * remove main from model_hooked
    
    * continued refactor
    
    * switch to logging package in cli
    
    * refactored logging and attributes
    
    * fix accidentially bad code from November
    
    * yolo integration fixes
    
    * improve hooks, saved results, configs
    
    * write some compatability for ultralytics
    
    * yolo test runs completable
    
    * fix to hook at split layer
    
    * tweak logging flag
    
    * notes for self
    
    * move onion dataloader
    
    * move yolo utils
    
    * add ultralytics to requirements.txt
    
    ---------
    
    Co-authored-by: nbovee <12849851+nbovee@users.noreply.github.com>
    Co-authored-by: Gopi-Krishna-Patapanchala <patapa89@students.rowan.edu>

--- src/experiment_design/services/basic_split_inference.py
+++ src/experiment_design/services/basic_split_inference.py
@@ -9,7 +9,7 @@ We also define a subclass of BaseDelegator to run the observer node.
 import logging
 import uuid
 
-from src.experiment_design.node_behavior.base import ParticipantService
+from src.experiment_design.services.base import ParticipantService
 import src.experiment_design.tasks.tasks as tasks
 
 
@@ -44,7 +44,7 @@ class ClientService(ParticipantService):
     def inference_sequence_per_input(self, task: tasks.SingleInputInferenceTask):
         assert self.model is not None
         input = task.input
-        splittable_layer_count = self.model.layer_count
+        splittable_layer_count = self.model.splittable_layer_count
 
         current_split_layer = 0
         while current_split_layer < splittable_layer_count:
